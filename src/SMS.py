076d1f4f147341d91570ff3d04f52aff9f34586c
Chris Hannam <bassdread@gmail.com>
2011-08-06 16:23:49 +0100

    working text sending

--- src/SMS.py
+++ src/SMS.py
@@ -4,6 +4,7 @@ import os, urllib
 from google.appengine.ext.webapp import template
 from google.appengine.api import urlfetch
 import base64
+from xml.etree import ElementTree
 
 class SendSMS(webapp.RequestHandler):
     
@@ -18,17 +19,23 @@ class SendSMS(webapp.RequestHandler):
         
         recipient = self.request.get('recipient')
         message = self.request.get('message')
-
+        template_values = {}
         if recipient and message:
             
             url = 'http://api.esendex.com/v1.0/messagedispatcher'
             encoded_username = base64.urlsafe_b64encode(account_settings['username']+':'+account_settings['password'])
             
             xml = """<?xml version='1.0' encoding='UTF-8'?><messages><accountreference>%s</accountreference><message><to>%s</to><body>%s</body></message></messages>""" % (account_settings['account'], recipient, message)  
-            result = urlfetch.fetch(url=url, method=urlfetch.POST, payload=xml, headers={'Content-Type' : 'text/xml', 'Authorization' : "Basic %s" % (encoded_username[:-1])})
-        
+            result = urlfetch.fetch(url=url, method=urlfetch.POST, payload=xml, headers={'Content-Type' : 'text/xml', 'Authorization' : "Basic %s" % (encoded_username)})
+
+            if result.status_code == 200:
+                root = ElementTree.fromstring(result.content)
+                template_values['message_id'] = root.getchildren()[0].attrib['id']
+                template_values['message'] = "Success!"
+                
+            
         path = os.path.join(os.path.dirname(__file__), 'templates/sent_sms.html')
-        self.response.out.write(template.render(path, {}))   
+        self.response.out.write(template.render(path, template_values))   
         
     
         
