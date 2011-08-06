abd84f7422a8348173097f4b0a3d1a515b2342e3
Chris Hannam <bassdread@gmail.com>
2011-08-06 14:06:35 +0100

    almost working sending

--- src/SMS.py
+++ src/SMS.py
@@ -2,6 +2,7 @@ from google.appengine.ext import webapp
 from local_settings import account_settings
 import os
 from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
 
 class SendSMS(webapp.RequestHandler):
     
@@ -14,6 +15,29 @@ class SendSMS(webapp.RequestHandler):
     def post(self):
         """Take the params and send the form"""
         
+        recipient = self.request.get('recipient')
+        message = self.request.get('message')
+
+        if recipient and message:
+            
+            url = 'https://www.esendex.com/secure/messenger/formpost/SendSMS.aspx'
+            
+            form_fields = {
+                'EsendexUsername' : account_settings['username'],
+                'EsendexPassword' : account_settings['password'],
+                'EsendexAccount' : account_settings['account'],
+                'EsendexRecipient' : recipient,
+                'EsendexBody' : message,
+                }
+            form_data = urllib.urlencode(form_fields)
+            
+            result = urlfetch.fetch(url=url, 
+                                    method=urlfetch.POST, 
+                                    payload=form_data, 
+                                    headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        
+        path = os.path.join(os.path.dirname(__file__), 'templates/sent_sms.html')
+        self.response.out.write(template.render(path, {}))   
         
     
         
