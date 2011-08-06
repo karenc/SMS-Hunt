373a4e4a7b785397696dac5f8f71c5fcbe0bbd4a
Rich Daley <rich@richd.me>
2011-08-06 16:51:51 +0100

    Yay! Each team allocated clues in random order

--- src/Hunt.py
+++ src/Hunt.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import db
 from google.appengine.api import users
+import random
 
 class Hunt(db.Model):
     """Parent object of each treasure hunt"""
@@ -11,6 +12,13 @@ class Hunt(db.Model):
         """Add a new clue to this hunt."""
         c = Clue(hunt=self,question=q,answer=a)
         c.put()
+        return c
+
+    def add_team(self,n,p):
+        """Add a new team to this hunt."""
+        t = Team(hunt=self,name=n,phone=p,clue_keys=[])
+        t.put()
+        return t
 
 class Clue(db.Model):
     """Each treasure hunt has multiple clues, each with a question and
@@ -20,3 +28,27 @@ class Clue(db.Model):
     question = db.StringProperty()
     answer   = db.StringProperty()
 
+class Team(db.Model):
+    """A team searching for answers to a given hunt."""
+    
+    hunt     = db.ReferenceProperty(Hunt, collection_name='teams')
+    name     = db.StringProperty()
+    phone    = db.StringProperty()
+
+    # This is the full list of remaining clues for this team. Populate
+    # initially with reset_clues
+    clue_keys    = db.ListProperty(int, default=[])
+
+    def reset_clues(self):
+        """Populate clue_keys with a shuffled list of keys for the current clues"""
+        cids = [c.key().id() for c in self.hunt.clues]
+        random.shuffle(cids)
+        self.clue_keys = cids
+        self.put()
+
+    def clues(self):
+        return [Clue.get_by_id(id) for id in self.clue_keys]
+
+    def current_clue(self):
+        """Get the current clue this team must solve"""
+        return self.clues()[0]
--- src/rich-testenv.py
+++ src/rich-testenv.py
@@ -1,19 +1,32 @@
 from google.appengine.ext import webapp
 from Hunt import Hunt
 
+# Create a hunt
 h = Hunt(name="Test hunt")
 h.put()
-h.add_clue("The hotel's name", "Mint")
-h.add_clue("Leeds ____?", "Hack")
+
+# Add some clues to the hunt
+h.add_clue("Chair colour", "purple")
+h.add_clue("Hotel name", "Mint")
+h.add_clue("#leeds____?", "hack")
+h.add_clue("Rich's Twitter name", "pedantic_git")
+
+# Add some teams
+h.add_team("Foo", "071234567").reset_clues()
+h.add_team("Bar", "072345678").reset_clues()
+h.add_team("Baz", "073456789").reset_clues()
 
 print "Content-Type: text/plain\n"
 
 print "Hunt test!\n"
 print "Hunt name: %s\n" % h.name
 
-for c in h.clues:
-    print "Clue %d:" % c.key().id()
-    print "Question: %s" % c.question
-    print "Answer: %s" % c.answer
+for t in h.teams:
+    print "Team: %s" % t.name
+    print "Clues in order:"
+    for c in t.clues():
+        print "  - %s" % c.question
     print ""
-
+    print "Current clue: %s" % t.current_clue().question
+    print ""
+    
